d0427c9b2862a9592c5838215e8bd49b62e626a5
Bartosz Koza <bartoszkoza09@gmail.com>
2024-05-07 18:09:36 +0200

    backend setup

---
+++ frontend/app/[id]/page.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { api_id } from "@/lib/api";
+import { PostType } from "@/lib/types";
+import { useQuery } from "@tanstack/react-query";
+import { detailsDummy } from "@/components/detailsDummy";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import React from "react";
+
+const Details = ({ params }: { params: { id: string } }) => {
+  // const { data, isLoading } = useQuery<PostType>({
+  //   queryKey: ["exer_detail"],
+  //   queryFn: () => api_id(params.id),
+  // });
+
+  const data = detailsDummy;
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className="container mx-auto px-4 py-8 flex flex-col md:flex-row mt-[11vh]"
+    >
+      <div className="md:w-1/2 flex flex-col justify-between md:mr-8">
+        <motion.h1
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className="text-3xl md:text-4xl lg:text-5xl font-bold mb-10"
+        >
+          {data?.name}
+        </motion.h1>
+        <div className="mb-8">
+          <h2 className="text-xl md:text-2xl font-bold mb-2">
+            Equipment: <span className="text-black">{data?.equipment}</span>
+          </h2>
+          <h2 className="text-xl md:text-2xl font-bold mb-2">
+            Body part: <span className="text-black-600">{data?.bodyPart}</span>
+          </h2>
+          <h2 className="text-xl md:text-2xl font-bold mb-2">
+            Target: <span className="text-black-600">{data?.target}</span>
+          </h2>
+          <h2 className="text-xl md:text-2xl font-bold mb-2">
+            SECONDARY MUSCLES:{" "}
+            <span className="text-black">{data?.secondaryMuscles}</span>
+          </h2>
+        </div>
+        <div>
+          <h1 className="text-2xl md:text-4xl font-bold mb-4">INSTRUCTIONS</h1>
+          <motion.ul
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.2 }}
+            className="text-lg md:text-2xl leading-relaxed list-decimal"
+          >
+            {data?.instructions.map((item, index) => (
+              <motion.li
+                key={index}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: index * 0.1 }}
+              >
+                {item}
+              </motion.li>
+            ))}
+          </motion.ul>
+        </div>
+      </div>
+      <div className="md:w-1/2 flex justify-center">
+        <motion.img
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.2 }}
+          className=""
+          height={500}
+          width={600}
+          src={data?.gifUrl}
+          alt="Exercise GIF"
+        />
+      </div>
+    </motion.div>
+  );
+};
+
+export default Details;
---
+++ frontend/lib/api.ts
@@ -0,0 +1,93 @@
+"use client";
+
+import axios from "axios";
+
+export async function apiCall() {
+  const options = {
+    method: "GET",
+    url: "https://exercisedb.p.rapidapi.com/exercises/bodyPart/back",
+    params: { limit: "1000000000000000000000" },
+    headers: {
+      "X-RapidAPI-Key": "41ae094c6bmsh4003a4f5b01146ep1405f9jsn427ca90d325d",
+      "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    },
+  };
+  const response = await axios.request(options);
+  return response.data;
+}
+
+export async function api_id(id: string) {
+  const options = {
+    method: "GET",
+    url: `https://exercisedb.p.rapidapi.com/exercises/exercise/${id}`,
+    headers: {
+      "X-RapidAPI-Key": "41ae094c6bmsh4003a4f5b01146ep1405f9jsn427ca90d325d",
+      "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    },
+  };
+
+  const response = await axios.request(options);
+  console.log(response.data)
+  return response.data;
+}
+
+
+export async function api_name(name: string){
+  const options = {
+    method: 'GET',
+    url: `https://exercisedb.p.rapidapi.com/exercises/name/${name}`,
+    params: {limit: '1000000000000000000000'},
+    headers: {
+      'X-RapidAPI-Key': '41ae094c6bmsh4003a4f5b01146ep1405f9jsn427ca90d325d',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+  
+    const response = await axios.request(options);
+    return response.data
+
+}
+
+export async function api_target(target: string) {
+  const options = {
+    method: 'GET',
+    url: `https://exercisedb.p.rapidapi.com/exercises/target/${target}`,
+    params: {limit: '1000000000000000000000'},
+    headers: {
+      'X-RapidAPI-Key': '41ae094c6bmsh4003a4f5b01146ep1405f9jsn427ca90d325d',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+  
+    const response = await axios.request(options);
+    return response.data
+}
+
+export async function api_body(body:string) {
+  const options = {
+    method: 'GET',
+    url: `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${body}`,
+    params: {limit: '1000000000000000000000'},
+    headers: {
+      'X-RapidAPI-Key': '41ae094c6bmsh4003a4f5b01146ep1405f9jsn427ca90d325d',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+  
+    const response = await axios.request(options);
+    return response.data
+}
+
+export async function api_type(type:string) {
+  const options = {
+    method: 'GET',
+    url: `https://exercisedb.p.rapidapi.com/exercises/equipment/${type}`,
+    params: {limit: '1000000000000000000000'},
+    headers: {
+      'X-RapidAPI-Key': '41ae094c6bmsh4003a4f5b01146ep1405f9jsn427ca90d325d',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+    const response = await axios.request(options);
+    return response.data
+}
