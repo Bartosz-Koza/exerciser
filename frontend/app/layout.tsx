04428c1896ecd95d22c73ac8149336da28b9f405
Bartosz Koza <bartoszkoza09@gmail.com>
2024-05-09 15:42:40 +0200

    authProvider i api (nie skończone)

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,8 @@ import Navbar from "@/components/navbar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ReactQueryProvider from '../providers/ReactQueryProvider'
+import ReactQueryProvider from "../providers/ReactQueryProvider";
+import AuthProvider from "@/providers/AuthProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,8 +20,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className}`}>
         <ReactQueryProvider>
-        <Navbar />
-        <main className="mt-[80px]">{children}</main>
+          <AuthProvider>
+            <Navbar />
+            <main className="mt-[80px]">{children}</main>
+          </AuthProvider>
         </ReactQueryProvider>
       </body>
     </html>
